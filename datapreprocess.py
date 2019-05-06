3c90adbe61815b7c10d69b013ed9d24b65a5fbd1
surajsoni5 <32407620+surajsoni5@users.noreply.github.com>
2019-05-06 05:59:06 +0530

    push jaggu

--- datapreprocess.py
+++ datapreprocess.py
@@ -1,18 +1,22 @@
+"""
+Converts the training data into hdf5 file format, for efficiency during training.
+"""
 import h5py
 import os
 import numpy as np
 import torchfile as tf
 from PIL import Image
 from matplotlib.pyplot import imshow
-cub_dataset='C:/Users/hello/Desktop/Accads/CUB_200_2011/'
-captions_dataset='C:/Users/hello/Desktop/Accads/cvpr2016_cub/word_c10/'
+import sys
+cub_dataset    = sys.argv[1] #'C:/Users/hello/Desktop/Accads/CUB_200_2011/' # Location of cub dataset 
+captions_dataset= sys.argv[2] # 'C:/Users/hello/Desktop/Accads/cvpr2016_cub/word_c10/' # Location of captions dataset
 VocabSize=5725+1 # Starts from 1 and ends at 5725, include start token also
 start=0 # 0 is start token
 end=1 # 1 is end token
 with h5py.File(os.path.join('dataset.hdf5'), 'w') as h:
     h['capsperimage']=10
-    maxcapslen=30
-    images = h.create_dataset('images', (11788, 3, 256, 256), dtype='uint8')
+    maxcapslen=30 # Max size of captions
+    images = h.create_dataset('images', (11788, 3, 256, 256), dtype='uint8') 
     captions = h.create_dataset('captions', (11788 ,10,maxcapslen+2), dtype='int32')
     class_ =  h.create_dataset('class', (11788,), dtype='int32')
     prev=''
@@ -25,7 +29,7 @@ with h5py.File(os.path.join('dataset.hdf5'), 'w') as h:
             img = img.resize((256,256))
             img = np.asarray( img, dtype='int32' )
             # The resnet accepts 3*w*w
-            if img.shape!=(256,256,3):  # Ask TAs what to do?
+            if img.shape!=(256,256,3):  # A few of the images are black and white
                 img=np.array([img,img,img])
                 print(line)
             else:
@@ -41,6 +45,3 @@ with h5py.File(os.path.join('dataset.hdf5'), 'w') as h:
                 last =last+temp.shape[0]
             class_[i]=int(line[:3])
     h['numcaptions']=i*10 
-
-if __name__=='__main__':
-    pass
--- datasets.py
+++ datasets.py
@@ -1,21 +1,21 @@
+"""
+Constructs the dataset from hdf5 file, which was created using datapreprocess script
+"""
 import torch
 from torch.utils.data import Dataset
 import h5py
 import numpy as np
 
-class CaptionDataset(Dataset): # Confirm about speed and stuff.. Ask TAs a better way
-
+class CaptionDataset(Dataset):
     def __init__(self, transform):
-        # self.h = h5py.File('drive/My Drive/CV_Project/dataset.hdf5','r',driver='core')
         self.h = h5py.File('dataset.hdf5','r',driver='core')
         self.capsperimage =  self.h['capsperimage'].value
-        self.images = np.array(self.h['images'])  # To completly load or not load is the question
+        self.images = np.array(self.h['images'])  
         self.captions= np.array(self.h['captions'])
         self.class_k =np.array(self.h['class'])
-        self.maxcapslen=30+3 #FIXME
+        self.maxcapslen=30+3 
         self.numcaptions=self.h['numcaptions'].value
         self.transform=transform
-        # print(self.capsperimage,self.captions,self.class_,self.numcaptions)
 
     def __getitem__(self, i):
         img = torch.FloatTensor(self.images[i // self.capsperimage] / 255.)
@@ -23,14 +23,10 @@ class CaptionDataset(Dataset): # Confirm about speed and stuff.. Ask TAs a bette
         caption = torch.LongTensor(self.captions[i//self.capsperimage,i%self.capsperimage])
         caplen = torch.LongTensor([self.maxcapslen- sum(caption==1)])
         class_k = torch.LongTensor([self.class_k[i // self.capsperimage]])
-        if caplen<2:
-            print(caplen,class_k,i)
         return img,caption,caplen,class_k
-
 
     def __len__(self): 
         return self.numcaptions
 
-if __name__=='__main__':
-    # Check if this is working properly
+if __name__ == "__main__":
     pass
